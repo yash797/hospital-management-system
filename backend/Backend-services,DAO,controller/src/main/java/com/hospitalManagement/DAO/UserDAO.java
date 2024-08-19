6d662dbb7e279e99ef1182a24c996eeb993e5dee
rhutuj <rhutujbhoir@gmail.com>
2024-08-19 21:27:57 +0530

    Deleted unneccesary folders

--- backend/Backend-services,DAO,controller/src/main/java/com/hospitalManagement/DAO/UserDAO.java
+++
@@ -1,8 +0,0 @@
-package com.hospitalManagement.DAO;
-
-import java.sql.SQLException;
-
-public interface UserDAO {
-    public User getUserByUsernameAndPassword(String username, String password) throws SQLException;
-
-}
